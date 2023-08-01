12868329299188b2afcb8239d54c2d3e2550a1b7
raviparihar-rv <59733114+raviparihar-rv@users.noreply.github.com>
2023-08-01 21:57:05 +0530

    add first commit

---
+++ models/post.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Post = db.define("Post", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  subTitle: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Post;
+ss;
