15d364ea37fa5eedd95a43a87ac06f379da2f439
raviparihar-rv <59733114+raviparihar-rv@users.noreply.github.com>
2023-08-28 00:07:46 +0530

    change user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const apiResponse = require("../utils/apiResponse");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const registerUser = async (req, res) => {
   try {
